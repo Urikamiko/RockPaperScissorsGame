a8d532f0d7c66aa57d8ccc984da4a08263e883c0
Ирина <@IRINA>
2024-06-18 22:34:17 +0700

    Add game logic

--- RockPaperScissorsGame/RockPaperScissorsGame/MainWindow.xaml.cs
+++ RockPaperScissorsGame/RockPaperScissorsGame/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace RockPaperScissorsGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        RPSGame game;
         public MainWindow()
         {
             InitializeComponent();
+            game = new RPSGame();
+        }
+
+        private void StoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            int result = game.Run(StoneButton.Content.ToString());
+            UserChoiceLabel.Content = StoneButton.Content.ToString();
+            CompChoiceLabel.Content = game.compChoice;
+            CheckResult(result);
+        }
+
+        private void ScissorsButton_Click(object sender, RoutedEventArgs e)
+        {
+            int result = game.Run(ScissorsButton.Content.ToString());
+            UserChoiceLabel.Content = ScissorsButton.Content.ToString();
+            CompChoiceLabel.Content = game.compChoice;
+            CheckResult(result);
+        }
+
+        private void PaperButton_Click(object sender, RoutedEventArgs e)
+        {
+            int result = game.Run(PaperButton.Content.ToString());
+            UserChoiceLabel.Content = PaperButton.Content.ToString();
+            CompChoiceLabel.Content = game.compChoice;
+            CheckResult(result);
+        }
+        private void CheckResult(int result)
+        {
+            switch (result)
+            {
+                case -1:
+                    ResultLabel.Content = "Вы проиграли!";
+                    break;
+                case 0:
+                    ResultLabel.Content = "Ничья!";
+                    break;
+                case 1:
+                    ResultLabel.Content = "Вы победили!";
+                    break;
+                default:
+                    ResultLabel.Content = "";
+                    break;
+            }
         }
     }
 }
---
+++ RockPaperScissorsGame/RockPaperScissorsGame/RPSGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsGame
+{
+    internal class RPSGame
+    {
+        List<string> compChoices = new List<string>() { "Камень", "Ножницы", "Бумага" };
+        Random rand = new Random();
+        public string compChoice { get; private set; }
+
+        public RPSGame() { }
+        public int Run(string userChoice) {
+            compChoice = compChoices[rand.Next(0,compChoices.Count)];
+            if (userChoice == compChoice) { return 0; }
+            if (userChoice == "Камень") {
+                if (compChoice == "Ножницы") {
+                    return 1;
+                }
+                if (compChoice == "Бумага")
+                {
+                    return -1;
+                }
+            }
+            if (userChoice == "Ножницы")
+            {
+                if (compChoice == "Камень")
+                {
+                    return -1;
+                }
+                if (compChoice == "Бумага")
+                {
+                    return 1;
+                }
+            }
+            if (userChoice == "Бумага")
+            {
+                if (compChoice == "Камень")
+                {
+                    return 1;
+                }
+                if (compChoice == "Ножницы")
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    }
+}
